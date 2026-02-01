1129b914509d509f90f60a9cc6f15452ef89ccaa
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-02-01 11:33:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/Aura_Hunter")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/+JSJvtrVjf780YTQ9")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Aura_Hunter")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -128,6 +128,7 @@ if SUPPORT_GROUP:
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
 )
         
+
 
 
 
